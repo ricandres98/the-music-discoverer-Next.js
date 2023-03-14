eef1116dbea6d09d2562ca74cc1419fa87854899
Ricardo <ricardo270298@gmail.com>
2023-03-13 22:10:36 -0400

    Seventh commit: - [STYLE] Some gradients were implemented in buttons, player and other elements

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -18,12 +18,13 @@ const BackButton = () => {
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 20 20"
         fill="currentColor"
-        class="w-5 h-5"
+        className="w-5 h-5"
+        strokeWidth="2"
       >
         <path
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
-          clip-rule="evenodd"
+          clipRule="evenodd"
         />
       </svg>
     </button>
